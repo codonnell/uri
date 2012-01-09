99a88f7d7925bae82e0e9192c73695f7d2e50d5e
Moritz Heidkamp <moritz@doo.net>
2012-01-09 16:47:57 +0100

    bump clojure version to 1.2.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject uri "1.0.1-SNAPSHOT"
   :description "A URI library for Clojure, wrapping the java.net.URI class."
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
