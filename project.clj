953b8ebe7585386263172cd4d66f5a06cc96be07
Chris O'Donnell <christopher.p.odonell@gmail.com>
2012-01-09 15:53:13 -0500

    Updated version to 1.1.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uri "1.0.1-SNAPSHOT"
+(defproject uri "1.1.0"
   :description "A URI library for Clojure, wrapping the java.net.URI class."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
